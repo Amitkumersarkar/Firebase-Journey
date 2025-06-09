c52e9073a19e3d540cc4617d5064ef86d15f1244
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-06-09 16:01:28 +0600

    added firebase file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <h3>Simple firebase</h3>
+    </>
+  )
+}
+
+export default App
---
+++ src/FireBase/Firebase.init.js
@@ -0,0 +1,20 @@
+// do not store config on the client side
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB11VRLGtQLxtKKDgI5wiJtyPA68vohuz8",
+    authDomain: "simple-firebase-3ee27.firebaseapp.com",
+    projectId: "simple-firebase-3ee27",
+    storageBucket: "simple-firebase-3ee27.firebasestorage.app",
+    messagingSenderId: "857086470273",
+    appId: "1:857086470273:web:e27ebbd557c8e2cbf44580"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
